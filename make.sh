9750b8bb904ef7a41c1740f4a10e07de97691653
Man Of Steel <69862555+ManOfDiamond@users.noreply.github.com>
2021-05-08 00:37:08 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -195,7 +195,7 @@ if [ "$outputtype" == "Aonly" ]; then
 fi
 
 date=`date +%Y%m%d`
-outputname="$romtypename-$outputtype-$sourcever-$date-nippongsi"
+outputname="$romtypename-$outputtype-$sourcever-$date-Diamond-gsi"
 outputimagename="$outputname".img
 outputtextname="$outputname".txt
 if [ "$4" == "" ]; then
